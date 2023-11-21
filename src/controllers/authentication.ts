c04e31cc695635ab6b5ef897f1319bd4c12a4851
alysonjan <alysonjan@gmail.com>
2023-11-21 23:25:44 +0800

    add error message

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -52,7 +52,8 @@ export const register = async (req: express.Request, res: express.Response) => {
     const existingUser = await getUserByEmail(email);
   
     if (existingUser) {
-      return res.sendStatus(400);
+
+      return res.status(400).json(  { "message" : "Account already exists."});
     }
 
     const salt = random();
